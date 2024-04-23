ab77c4efabfbc6855e6b8a5983d17d831bb33d99
fangyuan <fangyuan03@kuaishou.com>
2024-04-23 21:08:22 +0800

    up

--- data/eval.py
+++ data/eval.py
@@ -153,7 +153,7 @@ def eval_metrics_v2_from_tensors(
     )
     eval_ranks = torch.where(eval_rank_indices == k, MAX_K + 1, eval_rank_indices + 1)
     print('all_item_ids:', len(eval_state.all_item_ids), "k:", k, "max_k:", MAX_K)
-    print('eval_rank_indices:', eval_rank_indices.shape, eval_rank_indices)
+    print('eval_top_k_ids:', eval_top_k_ids.shape, eval_top_k_ids)
     print('target_ids:', target_ids.shape, target_ids)
     print('eval_ranks:', eval_ranks.shape, eval_ranks) 
     output = {
